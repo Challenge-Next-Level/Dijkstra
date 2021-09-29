ee32bfc3ba8d9b6797d9e8b9035b04ccd4ce9da9
jnl1128 <64057843+jnl1128@users.noreply.github.com>
2021-09-29 19:40:42 +0900

    Update 16165.py

--- individual/JungNarin/week01.Hash/16165.py
+++ individual/JungNarin/week01.Hash/16165.py
@@ -7,17 +7,18 @@ def girl_group():
 
     for n in range(N):
         members=[]
-        singer = input()
+        team = input()
         number = int(input())
         for num in range(number):
             members.append(input())
-        kpop_dict[singer]=sorted(members)
+        kpop_dict[team]= members
 
     for m in range(M):
         m1 = input()
         m2 = int(input())
         if m2 == 0:
-            print(*kpop_dict[m1], sep = "\n")
+            answer = sorted(kpop_dict[m1])
+            print(*answer, sep = "\n")
         else:
             for s in kpop_dict.keys():
                 if m1 in kpop_dict[s]:
