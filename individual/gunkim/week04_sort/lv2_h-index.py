8c622e89c3c3b30eaaf3c5d0ec559467e53e9f8c
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-10-09 12:38:48 +0900

    update h-index problem using one for loop idea

--- individual/gunkim/week04_sort/lv2_h-index.py
+++ individual/gunkim/week04_sort/lv2_h-index.py
@@ -1,18 +1,15 @@
+# 사실 다른 풀이(for문 2개 사용)로 통과는 했었다
+# 그런데 for문 하나라도 풀 수 있다는 것을 보게 되어 수정하게 되었다.
+# 하나의 for문 안에 문제의 조건문을 하나 걸어 만족시 반환을 하면 되었다.
+# 너무 단순하지만 내겐 너무 어려웠던 센스 있는 조건문이었다
 def solution(citations):
-    answer = 0
-    m = max(citations)
-    s = sorted(citations, reverse=True)
-    for i in range(m):
-        h_max, h_min = 0, 0
-        for j in s:
-            if j >= i:
-                h_max += 1
-            elif j <= i:
-                h_min += 1
-        if h_max >= i and h_min <= i and answer < i:
-            answer = i
-
-    return answer
+    length = len(citations)
+    s = sorted(citations)
+    # 적게 인용된 논문 부터 올라가면서 '인용된 횟수 값'이 '그 횟수만큼 인용된 논문 갯수 이상'이 되었을 때 return
+    for i in range(length):
+        if s[i] >= length - i:
+            return length - i
+    return 0
 
 
 c = [3, 0, 6, 1, 5]
