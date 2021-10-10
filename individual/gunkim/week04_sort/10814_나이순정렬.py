24afcdaa714d57ea2cbebb5f429bb1836cb5d548
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-10-10 16:32:12 +0900

    add comment

--- individual/gunkim/week04_sort/10814_나이순정렬.py
+++ individual/gunkim/week04_sort/10814_나이순정렬.py
@@ -7,10 +7,12 @@ def age_sort():
     answer = []
     for i in range(N):
         person = sys.stdin.readline().split()
+        # 뒤에서 나이를 기준으로 정렬을 해야하기 때문에 int로 형변환
         person[0] = int(person[0])
         answer.append(person)
     ans = sorted(answer, key=lambda x: x[0])
     for i in ans:
+        # join을 위해 int를 str로 형변환
         i[0] = str(i[0])
         print(' '.join(i))
     return
